d0b18a9f0ea8173038ed84173d5f7409994c1937
minq3010 <nmquoc204@gmail.com>
2025-06-27 22:02:43 +0700

    setting expo

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+
+export default function Root() {
+  return (
+    <>
+      <StatusBar style="dark" />
+      {/* Authentication provider */}
+      <Slot/>
+      {/* Authentication provider */}
+    </>
+  );
+}
