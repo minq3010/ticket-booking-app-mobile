10b4ab3aa0449c39f9d4fcac3848053cd368dd41
minq3010 <nmquoc204@gmail.com>
2025-06-30 17:11:24 +0700

    fix authentication check and redirect to login in layout component

--- app/(authed)/_layout.tsx
+++ app/(authed)/_layout.tsx
@@ -1,6 +1,12 @@
-// app/_layout.tsx
-import { Stack } from "expo-router";
+import { Redirect, Stack } from 'expo-router';
+import { useAuth } from '../../context/AuthContext';
 
-export default function Layout() {
+export default function AppLayout() {
+  const { isLoggedIn } = useAuth();
+
+  if (!isLoggedIn) {
+    return <Redirect href="/login" />;
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
